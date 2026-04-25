b7d77a631ac164a4206be1dc876432ee1801cbf4
elianiva <git@elianiva.my.id>
2026-04-26 00:38:24 +0700

    refactor: use frame instead of topbar

---
+++ src/components/Frame.svelte
@@ -0,0 +1,15 @@
+<div class="fixed top-0 left-0 w-full h-2 bg-pink-200 z-10"></div>
+<div class="fixed top-0 bottom-0 right-0 w-2 bg-pink-200 z-10"></div>
+<div class="fixed bottom-0 left-0 right-0 h-2 bg-pink-200 z-10"></div>
+<div class="fixed top-0 left-0 bottom-0 w-2 bg-pink-200 z-10"></div>
+<div class="fixed top-0 left-0 w-40 h-4 bg-sky-200 z-20"></div>
+<div
+	class="fixed top-0 left-0 w-20 h-6 bg-yellow-300 z-30 group cursor-pointer flex items-center justify-center transition-transform"
+>
+	<a
+		href="/notes"
+		class="invisible group-hover:visible text-xs text-yellow-700"
+	>
+		notes
+	</a>
+</div>
