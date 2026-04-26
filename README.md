b70f6ab1813484040aad7cb79d291fe03e2c8adc
elianiva <git@elianiva.my.id>
2026-04-26 22:14:29 +0700

    chore: start fresh

--- README.md
+++
@@ -1,6 +0,0 @@
-# elianiva.com
-
-Always a WIP :)
-
-![image](https://github.com/user-attachments/assets/cc246654-c66c-4076-b09a-51a0df370a14)
-
--- src/types/education.ts
+++
@@ -1,7 +0,0 @@
-export type Education = {
-	institution: string;
-	degree: string;
-	fieldOfStudy: string;
-	time: [start: Date, end: Date];
-	gpa: number;
-};
