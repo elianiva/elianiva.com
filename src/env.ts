8a239bbf0b7722c6651563702364ff367ba86f3b
elianiva <git@elianiva.my.id>
2026-04-27 18:05:57 +0700

    fix: env and things

--- src/env.ts
+++ src/env.ts
@@ -1,18 +1,14 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
+import { env as cfEnv } from "cloudflare:workers";
 
 export const env = createEnv({
   server: {
-    SERVER_URL: z.url().optional(),
     GH_TOKEN: z.string().min(1).optional(),
     NOTES_REPO: z.string().min(1).optional(),
     NOTES_OWNER: z.string().min(1).optional(),
     NOTES_BRANCH: z.string().min(1).optional(),
   },
-  clientPrefix: "VITE_",
-  client: {
-    VITE_APP_TITLE: z.string().min(1).optional(),
-  },
-  runtimeEnv: import.meta.env,
+  runtimeEnv: process.env || import.meta.env || cfEnv,
   emptyStringAsUndefined: true,
 });
